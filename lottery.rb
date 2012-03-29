48a00583d5c9f310bdf8f0f25554fd76975da5fb
= <=>
2012-03-29 14:36:00 -0700

    mostly working

--- lottery.rb
+++ lottery.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'nokogiri'
 require 'date'
 
-$debug = false
+$debug = true
 
+@picks = []
 
 def getPreviousDrawings startDate
     @dates = []
@@ -15,39 +15,30 @@ def getPreviousDrawings startDate
     @megafreq = {}
     @megaprob = {}
 
-    file = File.open 'megamillions.html'
-    htmldoc = Nokogiri::HTML(file)
-
-    items = htmldoc.css("div#scrollcontent div")
-
-    items.each do |item|
-        puts "I: " + item
-        dateshtml = item.css("div.num_date")
-        puts "A: " + dateshtml.text
-        dstr = dateshtml.text.gsub(/^.*>(\d+\/\d+\/\d+).*$/, '\1')
-        d = Date.strptime(dstr, "%m/%d/%Y")
-        puts d
-        if startDate < d
-            @dates.push(d)
-#            numbershtml = htmldoc.css("div.num_num")
-#            megahtml = htmldoc.css("div.num_mb")
+    lines = IO.readlines 'DownloadAllNumbers.txt'
+    lines.each do |line|
+        if line =~ /^\d+/
+            line =~ /^\d+\s*(.*\d{4})\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/
+            d = Date.parse($1)
+            if startDate < d
+                @dates.push(d)
+                @numbers.push([$2.to_i,$3.to_i,$4.to_i,$5.to_i,$6.to_i])
+                @megas.push $7.to_i
+            end
         end
     end
-#    numbershtml.each {|node| @numbers.push node.text.split('-')}
- #   megahtml.each {|node| @megas.push node.text.split(' ')[2].to_i}
-
-    puts @dates
-    file.close
+    puts @dates if $debug
 end
 
 def getNumberDistribution startDate
     getPreviousDrawings startDate
 
-    # maps of numbers 1-56 and their frequency
+    # maps of numbers 1-56 and their frequency, initialized to 0
+    (1..56).each { |x| @numberfreq[x] = 0 }
     @numbers.each do |numarray|
         numarray.each do |n|
             n = n.to_i
-            @numberfreq[n] = @numberfreq[n] ? @numberfreq[n] += 1 : 1
+            @numberfreq[n] += 1
         end
     end
 
@@ -57,42 +48,47 @@ def getNumberDistribution startDate
         @numberfreq.keys.sort.each do |key|
             if @numberfreq[key] == value
                 probability = (5.0 * value) / (56.0 * @numbers.length)
-                @numberprob[probability] = @numberprob[probability] ? @numberprob[probability].push(key) : @numberprob[probability] = [key]
+                @numberprob[probability] = @numberprob[probability] ? @numberprob[probability].push(key) : [key]
                 puts "Number: #{key}  - appeared #{value} times - probability #{probability}" if $debug
             end
         end
     end
 
-    puts "Probability Map: " if $debug
+    puts "Numbers Probability Map: " if $debug
     @numberprob.each do |key,value|
         puts "#{key} - #{value}" if $debug
     end
 
     # Do same for MegaBall
+    # maps of numbers 1-46 and their frequency, initialized to 0
+    (1..46).each { |x| @megafreq[x] = 0 }
     @megas.each do |n|
-        @megafreq[n] = @megafreq[n]? @megafreq[n] += 1 : 1
+        @megafreq[n] += 1
     end
     @megafreq.values.uniq.sort.each do |value|
         # Print the corresponding key
         @megafreq.keys.sort.each do |key|
             if @megafreq[key] == value
-                prob = value / (46.0 * @megas.length)
-                @megaprob[prob] = key
-                puts "Mega Number: #{key}  - appeared #{value} times - probability #{prob}" if $debug
+                probability = value / (46.0 * @megas.length)
+                @megaprob[probability] = @megaprob[probability] ? @megaprob[probability].push(key) : [key]
+                puts "Mega Number: #{key}  - appeared #{value} times - probability #{probability}" if $debug
             end
         end
     end
+    puts "Mega Probability Map: " if $debug
+    @megaprob.each do |key,value|
+        puts "#{key} - #{value}" if $debug
+    end
 end
 
-def pickNumbersBasedOnDistributionForDateRange startDate
-    # Initialize
-    getNumberDistribution startDate
 
+def pickNumbers
     # Start with 5 rows of mixtures of most popular numbers picked at random but weighted against their probability
     # 1. Sum the weights, i.e. probabilities
     totalweight = 0
     @numberprob.keys.each { |key| totalweight += key }
 
+    puts "Picking Numbers...." if $debug
     pick = []
     numberprob = @numberprob
     for i in 0..4
@@ -116,9 +112,7 @@ def pickNumbersBasedOnDistributionForDateRange startDate
             rnd -= prob
         end
     end
-    puts "Picked: "
-    puts pick.sort
-
+    print "Picked: " + pick.sort.to_s if $debug
     # Pick the Mega
     totalweight = 0.0
     @megaprob.keys.each { |key| totalweight += key }
@@ -128,18 +122,161 @@ def pickNumbersBasedOnDistributionForDateRange startDate
     @megaprob.keys.sort.reverse.each do |prob|
         if rnd <= prob
             puts "matched prob #{prob} with rnd #{rnd}" if $debug
-            puts "#{@megaprob[prob]} - Mega\n\n"
+            puts " - Mega = #{@megaprob[prob]}\n" if $debug
+            pick.push @megaprob[prob]
             break
         end
         rnd -= prob
     end
+    @picks.push pick
+end
+
+def pickWinningNumbers
+    firstFive = pickNumbers @numberprob, 5
+    mega = pickNumbers @megaprob, 1
+
+    @picks.push (firstFive + mega)
+end
+
+def pickNumbersBasedOnDistribution startDate
+    # Initialize
+    getNumberDistribution startDate
+
+    pickNumbers
+end
+
+def getNumberAgeArray
+    # initialize the hash of numbers with their number of weeks since seen
+    numberAge = {}
+    for i in 0 .. (@numbers.length-1)
+        @numbers[i].each do |n|
+            if !numberAge[n]
+                numberAge[n] = i
+            end
+        end
+    end
+    # create entries for any numbers that havent been seen
+    (0..55).each do |i| 
+        if !numberAge[i]
+            numberAge[i] = @dates.length
+        end
+    end
+    puts "Age Map" if $debug
+    puts numberAge if $debug
+    return numberAge
+end
+
+def updateNumberProb newnumberprob
+    puts "Old Probability Map: " if $debug
+    @numberprob.each do |key,value|
+        puts "#{key} - #{value}" if $debug
+    end
+
+    @numberprob = newnumberprob
+
+    puts "New Probability Map: " if $debug
+    @numberprob.each do |key,value|
+        puts "#{key} - #{value}" if $debug
+    end
+end
+
+def getNumbersThatHaveNotAppearedInAgesLinear startDate
+    # Initialize
+    getNumberDistribution startDate
+    numberAge = getNumberAgeArray
+    newnumberprob = {}
+
+    @numberprob.keys.each do |key|
+        @numberprob[key].each do |num|
+            # for each number in the array, create a new probability matrix with modified probabilities
+            # Magic number = weight * number of weeks since seen
+            newProb = key + (key * numberAge[num])
+            newnumberprob[newProb] = newnumberprob[newProb] ? newnumberprob[newProb].push(num) : [num]
+            puts "Number: #{num} - Old prob = #{key} - New Prob = #{key} + (#{key} * #{numberAge[num]}) = #{newProb}" if $debug 
+        end
+    end
+
+    updateNumberProb newnumberprob
+
+    pickNumbers
+end
+
+def getNumbersThatHaveNotAppearedInAgesExponential startDate
+    # Initialize
+    getNumberDistribution startDate
+    numberAge = getNumberAgeArray
+
+    newnumberprob = {}
+
+    @numberprob.keys.each do |key|
+        @numberprob[key].each do |num|
+            # for each number in the array, create a new probability matrix with modified probabilities
+            # Magic number = (2 ^ (0.000025 * x) ) -1
+            newProb = key + (2 ** (0.000025 * numberAge[num]) ) -1
+            newnumberprob[newProb] = newnumberprob[newProb] ? newnumberprob[newProb].push(num) : [num]
+            puts "Number: #{num} - Old prob = #{key} - New Prob = #{key} + 2 ^ (0.000025 * #{numberAge[num]}) -1 = #{newProb}" if $debug
+        end
+    end
+
+    updateNumberProb newnumberprob
+
+    pickNumbers
 end
 
 
-def getClosestAssoiciations 
+def getNumbersThatHaveNotAppearedInAgesLogarithmic startDate
+    # Initialize
+    getNumberDistribution startDate
+    numberAge = getNumberAgeArray
 
+    newnumberprob = {}
+
+    @numberprob.keys.each do |key|
+        @numberprob[key].each do |num|
+            # for each number in the array, create a new probability matrix with modified probabilities
+            # Magic number = (log2(x))*0.001
+            newProb = key + (Math.log2(numberAge[num] + 1) * 0.001)
+            newnumberprob[newProb] = newnumberprob[newProb] ? newnumberprob[newProb].push(num) : [num]
+            puts "Number: #{num} - Old prob = #{key} - New Prob = #{key} + (log2(#{numberAge[num]}) * 0.001) = #{newProb}" if $debug 
+        end
+    end
+
+    updateNumberProb newnumberprob
+
+    pickNumbers
 end
 
+(0..0).each { pickNumbersBasedOnDistribution Date.new(2012,3,1) }
+exit 1
+(0..2).each { pickNumbersBasedOnDistribution Date.new(2010,6,1) }
+(0..2).each { pickNumbersBasedOnDistribution Date.new(2011,1,1) }
 
-pickNumbersBasedOnDistributionForDateRange Date.new(2012,3,1)
+(0..2).each { getNumbersThatHaveNotAppearedInAgesLinear Date.new(2005,1,1) }
+(0..2).each { getNumbersThatHaveNotAppearedInAgesLinear Date.new(2010,6,1) }
+(0..2).each { getNumbersThatHaveNotAppearedInAgesLinear Date.new(2011,1,1) }
+
+(0..2).each { getNumbersThatHaveNotAppearedInAgesExponential Date.new(2005,1,1) }
+(0..2).each { getNumbersThatHaveNotAppearedInAgesExponential Date.new(2010,6,1) }
+(0..2).each { getNumbersThatHaveNotAppearedInAgesExponential Date.new(2011,1,1) }
+
+(0..2).each { getNumbersThatHaveNotAppearedInAgesLogarithmic Date.new(2005,1,1) }
+(0..2).each { getNumbersThatHaveNotAppearedInAgesLogarithmic Date.new(2010,6,1) }
+(0..2).each { getNumbersThatHaveNotAppearedInAgesLogarithmic Date.new(2011,1,1) }
+
+
+puts "Check for Duplicates..."
+for i in 0..@picks.length
+    for j in (i+1) .. @picks.length
+        if @picks[i] == @picks[j]
+            puts "Duplicate Found: #{@picks[i]}"
+        end
+    end
+end
+
+puts "Picked numbers:"
+@picks.each do |pick|
+    (0..4).each {|x| print "#{pick[x]} : " }
+    puts " Mega = #{pick[5]}"
+end
+
 
